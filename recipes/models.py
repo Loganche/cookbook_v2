ef88ffa3efdda3a5b0126683c10882b602fcfc9b
irybtsov <rybtsov_i_v@mail.ru>
2021-03-08 14:21:00 +0300

    in Recipe models.py created Ingredient model

--- recipes/models.py
+++ recipes/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
+class Ingredient(models.Model):
+    name = name = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
+    # ingredients list
+
     name = models.CharField(max_length=50, null=True)
     description = models.TextField(null=True)
     status = models.BooleanField(default=False, null=True)
+
+    def __str__(self):
+        return self.name
