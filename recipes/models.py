1c41010a7547b0e28bd52bdd6b6290a5f43123b3
irybtsov <rybtsov_i_v@mail.ru>
2021-03-08 14:15:04 +0300

    Added Navbar, in Recipe models.py created Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    description = models.TextField(null=True)
+    status = models.BooleanField(default=False, null=True)
